38c39ec61ee4286ff3dc8d4cfe90709e4794c1ac
Irfish <2845960938@qq.com>
2023-11-18 12:36:37 +0800

    enter node Serializable

--- Assets/NodeGraph/Runtime/Flow/EnterNode.cs
+++ Assets/NodeGraph/Runtime/Flow/EnterNode.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace NodeGraph
 {
+    [Serializable]
     [NodeMenu("Nodes/Flow/Enter","入口")]
     public class EnterNode : BaseNode
     {
