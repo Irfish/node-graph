63a8bd6e1b3da37499ab178e2f87528467630fbf
Irfish <2845960938@qq.com>
2024-06-11 10:59:13 +0800

    修改资源路径

--- Assets/NodeGraph/Editor/Src/SrcDefine.cs
+++ Assets/NodeGraph/Editor/Src/SrcDefine.cs
@@ -2,9 +2,9 @@ namespace NodeGraph.Editor
 {
     public static class SrcDefine 
     {
-        public const string baseNodeStylePath = "Assets/Plugins/NodeGraph/Editor/Src/Styles/BaseNodeView.uss";
-        public const string graphBackGroundPath = "Assets/Plugins/NodeGraph/Editor/Src/Styles/GraphBackGround.uss";
-        public const string graphWindowStylePath = "Assets/Plugins/NodeGraph/Editor/Src/Styles/BaseGraphView.uss";
+        public const string baseNodeStylePath = "Assets/NodeGraph/Editor/Src/Styles/BaseNodeView.uss";
+        public const string graphBackGroundPath = "Assets/NodeGraph/Editor/Src/Styles/GraphBackGround.uss";
+        public const string graphWindowStylePath = "Assets/NodeGraph/Editor/Src/Styles/BaseGraphView.uss";
     }    
 }
 
